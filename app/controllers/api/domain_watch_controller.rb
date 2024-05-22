77a707a143d60f3fe8911d4c50e2624c5dc52fb1
Takuma <>
2024-05-22 21:06:11 +0900

    修正

--- app/controllers/api/domain_watch_controller.rb
+++ app/controllers/api/domain_watch_controller.rb
@@ -2,7 +2,6 @@ class Api::DomainWatchController < Api::BaseController
   skip_before_action :verify_authenticity_token
 
   def index
-    puts "Received params: #{params.inspect}"
     usecase = Api::DomainWatchUsecase.new(
       input: Api::DomainWatchUsecase::Input.new(
         domainName: params[:domainName]

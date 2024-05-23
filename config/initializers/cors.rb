fca6316ca2a5d81718b14ff3e6a919a39f6ac632
Takuma <>
2024-05-23 12:36:09 +0900

    cors.rbの修正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:9000'
+      origins 'https://domain-watcher.soru-tech.com'
   
       resource '*',
         headers: :any,
